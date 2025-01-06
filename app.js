b006880ebca59796fd7f46219f191a0f911facb3
costar011 <dnaer456@gmail.com>
2025-01-06 23:33:26 +0900

    let, const

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let a  = 5;
+const a  = 5;
 const b = 2;
 
-const myName = "costar";
+let myName = "costar";
 
 
 console.log(a) + b;
 console.log(a) * b;
 console.log(a) / b;
 console.log("Hello" + myName);
+
+myName = "costar";
