b1eb71b48b0ce7a7dcdd789a831debf59a950a96
costar011 <dnaer456@gmail.com>
2025-01-20 06:29:54 +0900

    2.10

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const player = {
-  name: "costar",
-  hello: function (otherPersonName) {
-    console.log("hello" + otherPersonName + "nice to meet you");
-  },
-};
+const buy = ["potato", "tomato", "cabbage"];
 
-player.hello("cocoa");
-player.hello("costar2");
+console.log(buy);
+buy[2] = "carrot";
+console.log(buy);
