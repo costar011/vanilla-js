79c7ba63ce946013bda5f9367f20afae7b803f6a
costar011 <dnaer456@gmail.com>
2025-01-25 21:27:33 +0900

    2.13

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-const calculator = {
-  plus: function(a, b) {
-    return a + b;
-  },
-  minus: function(a, b) {
-    return a - b;
-  },  
-  times: function(a, b) {
-    return a * b;
-  },    
-  divide: function(a, b) {
-    return a / b;
-  },  
-  power: function(a, b) {
-    return a ** b;
-  },  
-  
-};
+const age = prompt('How old are you?');
 
-const plusreuslt = calculator.plus(2, 3);
-const minusreuslt = calculator.plus(plusreuslt, 11);
-const timereuslt = calculator.plus(10, minusreuslt);
-const dividereuslt = calculator.plus(timereuslt, plusreuslt);
-const powerreuslt = calculator.plus(dividereuslt, minusreuslt);
+console.log(age, parseInt(age));
