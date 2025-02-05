67cfd28eafc83562a2acbeb23f52bf1e789c79ac
costar011 <dnaer456@gmail.com>
2025-02-05 23:01:01 +0900

    3.5

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ const title = document.querySelector("div.hello:first-child h1");
 function handleTitleClick() {
   title.style.color = "blue";
 }
-title.addEventListener("click", handleTitleClick);
+
+function handleMouseEnter() {
+  title.style.color = "Mousw is here !";
+}
+
+function handleMouseLeave() {
+  title.style.color = "Mouse is gone !";
+}
+
+function handleWindowResize() {
+ documentElement.style.backgroundColor = "tomato";
+}
+
+title.onclick("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseenter", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
