9e59cc5735b27c12bbd0671cd035cb868df9c485
costar011 <dnaer456@gmail.com>
2025-02-06 00:05:12 +0900

    3.6

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.title.style.color = "blue";
 }
 
 function handleMouseEnter() {
-  title.style.color = "Mousw is here !";
+  h1.title.style.color = "Mousw is here !";
 }
 
 function handleMouseLeave() {
-  title.style.color = "Mouse is gone !";
+  h1.title.style.color = "Mouse is gone !";
 }
 
 function handleWindowResize() {
  documentElement.style.backgroundColor = "tomato";
 }
 
-title.onclick("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseenter", handleMouseLeave);
+function handleWindowCopy() {
+  alert("copier");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD");
+}
+
+
+h1.onclick("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseenter", handleMouseLeave);
 
 window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
