7b2f1834fdc9deb9e432f138af46085ef1b9856d
costar011 <dnaer456@gmail.com>
2025-02-08 00:11:04 +0900

    3.6

--- app.js
+++ app.js
@@ -4,26 +4,4 @@ function handleTitleClick() {
   h1.title.style.color = "blue";
 }
 
-function handleMouseEnter() {
-  h1.title.style.color = "Mousw is here !";
-}
-
-function handleMouseLeave() {
-  h1.title.style.color = "Mouse is gone !";
-}
-
-function handleWindowResize() {
- documentElement.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD");
-}
+h1.addEventListener("click", handleTitleClick);
