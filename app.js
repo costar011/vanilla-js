ef3ae1eae03a51740206d74259019791f87e2ab9
costar011 <dnaer456@gmail.com>
2025-02-10 23:12:04 +0900

    3.6

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  h1.title.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
 }
 
 h1.addEventListener("click", handleTitleClick);
