a646483b1971adc0279698fc5f127882a8f415bb
costar011 <dnaer456@gmail.com>
2025-02-16 23:02:16 +0900

    app

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  if (h1.className === "clicked") {
-    h1.className = "";
-  } else {
-    h1.className = "clicked";
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
