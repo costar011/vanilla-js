898fb836a3c385170d14116070556a60cde0d71a
costar011 <dnaer456@gmail.com>
2025-02-23 22:09:27 +0900

    4.4

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
 function onLoginSubmit(event) {
- event.preventDefault();
-  console.log(username);
-}
-
-function handleLinkClick(event) {
   event.preventDefault();
-  console.dir(event);
+  logForm.classList.add("hidden");
+  const username = loginInput.value;
+  greeting.innerHTML = "Hello" + username;
+  greeting.classList.remove("hidden");
 }
-
 loginButton.addEventListener("submit", onLoginSubmit);
-handleLinkClick.addEventListener("click", handleLinkClick);
