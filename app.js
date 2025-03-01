5cb5b6d2c2e3631ec0376663e3410369edad7f20
costar011 <dnaer456@gmail.com>
2025-03-01 22:39:04 +0900

    4.6

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector(".greeting");
 greeting.hidden = true;
 
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  loginForm.hidden = true;
-  greeting.innerText = `Hello ${username}`;
-  greeting.hidden = false;
-});
+  localStorage.setItem("username", username);
+  paintGreetings(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+  // show the greetings
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = `Hello ${savedUsername}`;
+}
