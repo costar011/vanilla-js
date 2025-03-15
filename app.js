5716473779b29ac3aab95b09abc249bb95d0017d
costar011 <dnaer456@gmail.com>
2025-03-15 20:25:48 +0900

    4.6

--- app.js
+++ app.js
@@ -11,20 +11,24 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem("username", username);
-  paintGreetings(username);
+  paintGreetings(savedUsername);
+}
+
+function paintGreetings(username) {
+  greeting.innerHTML = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
+// 예를 들어 localStorage에 유저정보가 있으면 거기서 유저정보를 받아서 인자로 넣어주면 된다
+// 만약 localStorage에 유정정보가 없다면 form의 submit을 기다리고, form이 submit 되면 우리는 input으로부터 유저정보를 받고
+// input에서 받은 user를 가진 paintGreetings 함수를 호출할거다
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-}
-else {
-  // show the greetings
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerHTML = `Hello ${savedUsername}`;
+} else {
+  paintGreetings(savedUsername);
 }
